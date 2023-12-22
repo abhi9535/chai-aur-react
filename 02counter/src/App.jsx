6304ba8a85a07448269d8ea5127a2c10fe80e401
abhi9535 <abhi.aili1047@gmail.com>
2023-12-22 21:01:17 +0530

    counter updatiion

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, myCounter] = useState(0)
+
+
+   //let counter=15
+  
+   const addValue=()=>{
+      if(counter<20){
+        myCounter(counter+1)
+      }
+        
+   }
+
+   const removevalue=()=>{
+    if(counter>0){
+      myCounter(counter-1)
+    }
+   }
+
+  return (
+   <>
+   <h1>chai aur react</h1>
+   <br />
+   <h2>Counter value: {counter}</h2>
+   <br />
+   <button onClick={addValue}>Add value {counter}</button>
+   <br />
+   <button onClick={removevalue}>remove value{counter}</button>
+   </>
+  )
+}
+
+export default App
